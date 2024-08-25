20d1ad517c9b706ef932375e18068b6b8eef40c0
Miroslav Tsarev <mtzarev@gmail.com>
2024-08-25 22:03:58 +0300

    Add files via upload

---
+++ 2. Common Elements.js
@@ -0,0 +1,15 @@
+function commonElements(arr1, arr2) {
+    let newArr = [];
+    for (let i = 0; i < arr1.length; i++) {
+        let word = arr1[i];
+        if (arr2.includes(word)) {
+            newArr.push(word);
+        }
+    }
+    for (words of newArr) {
+        console.log(words);
+    }
+
+}
+commonElements(['S', 'o', 'f', 't', 'U', 'n', 'i', ' '],
+    ['s', 'o', 'c', 'i', 'a', 'l']);
